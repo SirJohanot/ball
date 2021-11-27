f768b4d810c61dbd4778a2ebf1c5afecb200c805
Johanot <67147458+SirJohanot@users.noreply.github.com>
2021-11-27 12:33:17 +0300

    added static final Strings, BallCreator, renamed and reformatetd test methods to follow conventions

--- src/test/java/com/epam/ball/logic/BallCreatorTest.java
+++ src/test/java/com/epam/ball/logic/BallCreatorTest.java
@@ -7,35 +7,28 @@ import org.junit.Test;
 
 public class BallCreatorTest {
 
+    private static final String VALID_LINE = "4.0 1.0 2.0 3.0";
+    private static final String INVALID_LINE = "-4.0 1.0 2.0 3.0";
+
+    private final BallCreator creator = new BallCreator();
+
     @Test
-    public void testShouldCreateABallWhenDataIsValid(){
+    public void testCreateShouldCreateABallWhenDataIsValid() {
         //given
-        BallCreator creator=new BallCreator();
-        double radius=4;
-        double xCoordinate=1;
-        double yCoordinate=2;
-        double zCoordinate=3;
-        String creatorLine="4.0 1.0 2.0 3.0";
+        Ball expectedBall = new Ball(4.0, new Point(1.0, 2.0, 3.0));
         //when
-        Ball ball=new Ball(radius, new Point(xCoordinate, yCoordinate, zCoordinate));
-        Ball createdBall=creator.create(creatorLine);
+        Ball createdBall = creator.create(VALID_LINE);
         //then
-        Assert.assertEquals(ball, createdBall);
+        Assert.assertEquals(expectedBall, createdBall);
     }
 
     @Test
-    public void testShouldCreateABallWhenDataIsInvalid(){
+    public void testCreateShouldCreateABallWhenDataIsInvalid() {
         //given
-        BallCreator creator=new BallCreator();
-        double radius=-4;
-        double xCoordinate=1;
-        double yCoordinate=2;
-        double zCoordinate=3;
-        String creatorLine="-4.0 1.0 2.0 3.0";
+        Ball expectedBall = new Ball(-4.0, new Point(1.0, 2.0, 3.0));
         //when
-        Ball ball=new Ball(radius, new Point(xCoordinate, yCoordinate, zCoordinate));
-        Ball createdBall=creator.create(creatorLine);
+        Ball createdBall = creator.create(INVALID_LINE);
         //then
-        Assert.assertEquals(ball, createdBall);
+        Assert.assertEquals(expectedBall, createdBall);
     }
 }
