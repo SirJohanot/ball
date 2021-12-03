c073e98fc06fb977a1899d9a571cb14c9f41542e
Johanot <67147458+SirJohanot@users.noreply.github.com>
2021-12-03 14:21:28 +0300

    initial observer and observable

---
+++ src/main/java/com/epam/ball/logic/BallStore.java
@@ -0,0 +1,36 @@
+package com.epam.ball.logic;
+
+import com.epam.ball.entity.BallObservable;
+import com.epam.ball.entity.Parameters;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BallStore implements Observer<BallObservable>{
+
+    private static BallStore INSTANCE;
+
+    private final BallCalculator ballCalculator = new BallCalculator();
+
+    private final Map<Integer, Parameters> parameters = new HashMap<>();
+
+    private BallStore() {
+    }
+
+    public void update(BallObservable ball) {
+        double volume = ballCalculator.calculateVolume(ball);
+        double surfaceArea = ballCalculator.calculateSurfaceArea(ball);
+        parameters.put(ball.getId(), new Parameters(volume, surfaceArea));
+    }
+
+    public Parameters get(Object key) {
+        return parameters.get(key);
+    }
+
+    public static BallStore getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new BallStore();
+        }
+        return INSTANCE;
+    }
+}
