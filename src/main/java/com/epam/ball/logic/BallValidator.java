b2aade03f320448a03a157ee21450ee8831b0d4c
Johanot <67147458+SirJohanot@users.noreply.github.com>
2021-12-26 03:59:34 +0300

    removed redundant no-argument constructor

--- src/main/java/com/epam/ball/logic/BallValidator.java
+++ src/main/java/com/epam/ball/logic/BallValidator.java
@@ -6,9 +6,6 @@ public class BallValidator {
 
     private static final String BALL_LINE_REGEX = "\\d+\\.\\d+( (-)?\\d+\\.\\d+){3}";
 
-    public BallValidator() {
-    }
-
     public boolean isValidLine(String line) {
         return Pattern.matches(BALL_LINE_REGEX, line);
     }
