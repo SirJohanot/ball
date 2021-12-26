33cb52598d17f4cfcde586af8bc7614a826fc599
Johanot <67147458+SirJohanot@users.noreply.github.com>
2021-12-26 04:00:07 +0300

    changed variable naming to use words instead of numbers

--- src/main/java/com/epam/ball/repository/CenterXCoordinateBallComparator.java
+++ src/main/java/com/epam/ball/repository/CenterXCoordinateBallComparator.java
@@ -7,11 +7,11 @@ import java.util.Comparator;
 
 public class CenterXCoordinateBallComparator implements Comparator<BallObservable> {
     @Override
-    public int compare(BallObservable o1, BallObservable o2) {
-        Point center1 = o1.getCenter();
-        Point center2 = o2.getCenter();
-        double xCoordinate1 = center1.getXCoordinate();
-        double xCoordinate2 = center2.getXCoordinate();
-        return Double.compare(xCoordinate1, xCoordinate2);
+    public int compare(BallObservable firstObservable, BallObservable secondObservable) {
+        Point firstObservableCenter = firstObservable.getCenter();
+        Point secondObservableCenter = secondObservable.getCenter();
+        double firstObservableXCoordinate = firstObservableCenter.getXCoordinate();
+        double secondObservableXCoordinate = secondObservableCenter.getXCoordinate();
+        return Double.compare(firstObservableXCoordinate, secondObservableXCoordinate);
     }
 }
