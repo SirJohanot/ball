c8020c72918deb754f45f5f7a2a42321a954ad11
Johanot <67147458+SirJohanot@users.noreply.github.com>
2021-12-26 04:03:34 +0300

    changed naming to use words instead of numbers

--- src/main/java/com/epam/ball/repository/RadiusBallComparator.java
+++ src/main/java/com/epam/ball/repository/RadiusBallComparator.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
 
 public class RadiusBallComparator implements Comparator<BallObservable> {
     @Override
-    public int compare(BallObservable o1, BallObservable o2) {
-        return Double.compare(o1.getRadius(), o2.getRadius());
+    public int compare(BallObservable firstObservable, BallObservable secondObservable) {
+        return Double.compare(firstObservable.getRadius(), secondObservable.getRadius());
     }
 }
